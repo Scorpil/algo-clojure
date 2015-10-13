537e06203ac5fb9e67cf2077c6513643081a2990
Scorpil <scorpilmail@gmail.com>
2015-10-14 00:23:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Clojure implementation of algorithms described in Coursera "[Algorithms: Design and Analysis](https://www.coursera.org/course/algo)" course.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject algo-clojure "0.1.0-SNAPSHOT"
+  :description (str "Clojure implementation of algorithms described in "
+                    "Coursera \"Algorithms: Design and Analysis\" course.")
+  :url "https://github.com/Scorpil/algo-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])

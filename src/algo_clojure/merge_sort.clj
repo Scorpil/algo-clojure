80ab744cac5d2ef695c31effb2b1c919cdff18f2
Scorpil <scorpilmail@gmail.com>
2015-10-18 12:46:48 +0200

    Merge sort algorithm

--- src/algo_clojure/merge_sort.clj
+++ src/algo_clojure/merge_sort.clj
@@ -1,5 +1,18 @@
 (ns algo-clojure.merge-sort)
 
+(defn combine
+  [xs1 xs2]
+  (if (empty? xs1) xs2 ; if one sequence is empty return other one
+      (if (empty? xs2) xs1
+          (let [[f1 & t1] xs1
+                [f2 & t2] xs2]
+            (if (< f1 f2) ; else, return smallest element and combine the rest
+              (cons f1 (combine t1 xs2))
+              (cons f2 (combine xs1 t2)))))))
+
 (defn merge-sort
   [xs]
-  nil)
+  (let [len-xs (count xs)]
+    (if (< len-xs 2) xs ; return sequence if there is less than 2 elements in it
+        (let [[xs1 xs2] (split-at (quot len-xs 2) xs)] ; split sequence in two
+          (combine (merge-sort xs1) (merge-sort xs2)))))) ; combine sorted parts
--- test/algo_clojure/merge_sort_test.clj
+++ test/algo_clojure/merge_sort_test.clj
@@ -3,26 +3,30 @@
             [algo-clojure.merge-sort :refer :all]))
 
 (deftest merge-sort-test
-  (testing "empty array"
+  (testing "empty sequence"
     (is (= (merge-sort (list))
            (list))))
 
-  (testing "array with one element"
+  (testing "sequence with one element"
     (is (= (merge-sort (list 1))
            (list 1))))
 
-  (testing "array with two elements"
+  (testing "sequence with two elements"
     (is (= (merge-sort (list 2 1))
            (list 1 2))))
 
-  (testing "array with unique elements"
+  (testing "sequence sorted from the start"
+    (is (= (merge-sort (list 1 2 3 4 5))
+           (list 1 2 3 4 5))))
+
+  (testing "sequence with unique elements"
     (is (= (merge-sort (list 5 3 1 2 4))
            (list 1 2 3 4 5))))
 
-  (testing "array with repeated elements"
+  (testing "sequence with repeated elements"
     (is (= (merge-sort (list 3 3 1 2 4))
            (list 1 2 3 3 4))))
 
-  (testing "array with negative elements"
+  (testing "sequence with negative elements"
     (is (= (merge-sort (list (- 4) (- 4) 4 1 2))
            (list (- 4) (- 4) 1 2 4)))))
